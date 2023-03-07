4db92e08940a3af4339c356c879b295465e934f6
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-07 10:39:04 +0900

    초기 설정

---
+++ src/test/java/com/example/newsns/NewSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
